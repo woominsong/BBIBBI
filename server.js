3a1550025516d5f6fd316b468460f1dd8032d4e3
totolacky <totolacky@kaist.ac.kr>
2020-06-14 22:27:56 +0900

    Change bcrypt to bcryptjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const socketio = require('socket.io');
 
 const app = express();
